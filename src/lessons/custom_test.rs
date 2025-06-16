1dad3dd32f757c3c187d80ec7e4c8dfaaa678407
Muhammad Fikri Syahid <fikrisyahid14@gmail.com>
2025-06-16 08:48:39 +0700

    chore: setup modular lessons structure

---
+++ src/lessons/custom_test.rs
@@ -0,0 +1,4 @@
+pub fn run() {
+    let x = [1, 2, 3, 4, 5];
+    println!("x: {:?}", x);
+}
--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,5 @@ mod lessons;
 
 fn main() {
     lessons::guess_the_number::run();
+    // lessons::custom_test::run();
 }
